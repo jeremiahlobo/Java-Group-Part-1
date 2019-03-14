216658d8cb8aabb387f9f69f2b0da4b23248b9c5
markojovanovicuser <44778616+markojovanovicuser@users.noreply.github.com>
2019-03-14 16:43:49 -0600

    startpage

---
+++ src/Base/Base.java
@@ -0,0 +1,28 @@
+package Base;
+
+import javafx.fxml.*;
+import javafx.scene.input.MouseEvent;
+
+public class Base {
+
+    @FXML
+    void AllCustomerMouseClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void AllPackagesMouseClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void AllProductsMouseClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void AllSuppliersMouseClick(MouseEvent event) {
+
+    }
+
+}
