5de02b1e34eb9bd4f77ae7345a677f9607eeff1e
Helen Lin <491198@ACDM.DS.SAIT.CA>
2019-04-21 12:20:09 -0600

    My updated stuff

--- src/Model/ProductModel.java
+++ src/Model/ProductModel.java
@@ -1,4 +1,46 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductModel extends DB {
+
+    //*************************************************************
+    //* GET ALL Products
+    //*************************************************************
+    public List getProducts()
+    {
+        List<String> ProductList = new ArrayList<>();
+
+        try {
+            //Connect to the DB
+            Connection DBConnection = getConnection();
+            //Create a statement
+            Statement stmt = DBConnection.createStatement();
+/*
+
+    private int ProductId;
+    private String ProdName;
+
+            */
+
+
+            String sql = "SELECT ProductId, ProdName FROM `products`";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                ProductList.add(rs.getString("ProductId"));
+                ProductList.add(rs.getString("ProdName"));
+
+                System.out.println();
+            }
+
+        } catch (Exception se){
+            se.getStackTrace();
+        }
+        return ProductList;
+    }
+
 }
