10c844bfa303f8cd1fb9494d234c7b5909aa0ae3
Helen Lin <491198@ACDM.DS.SAIT.CA>
2019-04-21 18:14:32 -0600

    Merge branch 'master' into HelenBranch4

--- src/Base/Main.java
+++ src/Base/Main.java
@@ -131,7 +131,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("basePage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
         primaryStage.setTitle("Dashboard");
         primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
